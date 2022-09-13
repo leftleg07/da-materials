88b3ea9b67701b5bcbd9449fcdfb143228cd4cdf
김은수 <galaxy.kim@hanwha.com>
2022-09-13 09:46:46 +0900

    intellij ide

---
+++ bin/da_materials.dart
@@ -0,0 +1,5 @@
+import 'package:da_materials/da_materials.dart' as da_materials;
+
+void main(List<String> arguments) {
+  print('Hello world: ${da_materials.calculate()}!');
+}
---
+++ test/da_materials_test.dart
@@ -0,0 +1,8 @@
+import 'package:da_materials/da_materials.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
